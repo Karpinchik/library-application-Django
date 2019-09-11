1f1f6c71da1ec84b4cc21c0bff8613cb8458f76a
Karpinchik <45141044+Karpinchik@users.noreply.github.com>
2019-09-11 13:26:57 +0300

    Заменил sql на sqlite3

--- test_project/settings.py
+++ test_project/settings.py
@@ -66,21 +66,10 @@ WSGI_APPLICATION = 'test_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'django_test',
-        'USER': 'karpin_admin',
-        'PASSWORD': 'karpin',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
